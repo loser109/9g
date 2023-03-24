3256e1215321dc8a342435741b1b261b55da5647
loser109 <47694097+loser109@users.noreply.github.com>
2023-03-24 11:26:50 +0800

    Update patch_qudao.sh

--- patch_qudao.sh
+++ patch_qudao.sh
@@ -8,7 +8,7 @@ download_azurlane () {
     # 这个链接是MUMU下载的,应该是9游,其他渠道自行修改直链
     #url="https://downali.game.uc.cn/s1/2/10/20230213150150_blhx_uc_2022_11_02_18_24_01.apk?x-oss-process=udf/uc-apk,ZBHDhDR0LVBkTsK*wpLCng==afae37c2a88fd1ca&sh=10&sf=1831727323&vh=18330f93bd450707942ce0b882a0c6b2&cc=2521889677&did=217b729f8a4841dd81901816dfba551f"
     #这个链接是当乐网
-    url="https://downali.game.uc.cn/s/2/10/20230213150150_blhx_uc_2022_11_02_18_24_01.apk?x-oss-process=udf/uc-apk,ZBHDgDZ0LxUV2e1761983120100d&did=e42d74da93fc4e4ab419dde1a6d5a663&cc=1976653420&vh=18330f93bd450707942ce0b882a0c6b2&sf=1831723280"
+    url="https://downali.game.uc.cn/s/2/10/20230213150150_blhx_uc_2022_11_02_18_24_01.apk?x-oss-process=udf/uc-apk,ZBHDgDZ0LxUV2e1761983120100d&did=d13c8a901a7b45038d97f89bc607181c&cc=1976653420&vh=18330f93bd450707942ce0b882a0c6b2&sf=1831723280"
     # 使用wget命令下载apk文件
     curl -o blhx.apk  $url
     fi
